9d4968e3362863fbbafc88aaace1bd0b55199143
Jonas Birmé <jonas.birme@eyevinn.se>
2023-11-14 14:50:27 +0100

    feat: support for destination type 'void'

---
+++ src/logger.ts
@@ -0,0 +1,30 @@
+import { ILogger } from '@eyevinn/hls-pull-push/types';
+import { Logger } from '@pieropatron/tinylogger';
+
+export class MyLogger implements ILogger {
+  private logger: Logger;
+
+  constructor(nodeEnv?: string) {
+    this.logger = new Logger('channel-engine-bridge');
+    this.logger.level = 'debug';
+    if (nodeEnv && nodeEnv === 'production') {
+      this.logger.level = 'info';
+    }
+  }
+
+  info(message: string) {
+    this.logger.info(message);
+  }
+
+  verbose(message: string) {
+    this.logger.debug(message);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  error(message: string) {
+    this.logger.error(message);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,48 @@
+import { HLSPullPush, VoidOutput } from '@eyevinn/hls-pull-push';
 import api from './api';
+import { MyLogger } from './logger';
 
-const server = api({ title: '@eyevinn/typescript-nodejs' });
+const server = api({ title: 'channel-engine-brdige' });
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
+
+if (!process.env.SOURCE) {
+  console.error('Missing environment variable SOURCE');
+  process.exit(1);
+}
+
+const destinationType = process.env.DEST_TYPE || 'void';
 
 server.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     throw err;
   }
   console.log(`Server listening on ${address}`);
+
+  const source = new URL(
+    process.env.SOURCE ||
+      'https://demo.vc.eyevinn.technology/channels/demo/master.m3u8'
+  );
+
+  const pullPushService = new HLSPullPush(new MyLogger(process.env.NODE_ENV));
+  let outputDest;
+  if (destinationType == 'void') {
+    const voidOutputPlugin = new VoidOutput();
+    pullPushService.registerPlugin(destinationType, voidOutputPlugin);
+    outputDest = voidOutputPlugin.createOutputDestination(
+      undefined,
+      pullPushService.getLogger()
+    );
+  }
+  if (outputDest) {
+    const sessionId = pullPushService.startFetcher({
+      name: 'default',
+      url: source.toString(),
+      destPlugin: outputDest,
+      destPluginName: destinationType
+    });
+    outputDest.attachSessionId(sessionId);
+  }
 });
 
 export default server;
